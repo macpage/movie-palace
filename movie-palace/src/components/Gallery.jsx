722c892cfebc61c83bb94a3951bd9a2350e2788b
Mac <sophanpa@gmail.com>
2023-07-17 23:34:35 +0200

    Edit

--- movie-palace/src/components/Gallery.jsx
+++ movie-palace/src/components/Gallery.jsx
@@ -3,7 +3,7 @@ import Movie from "./Movie";
 function Gallery(){
     const array = [1,2,3,4,5,6,7,8,9,10];
     return <div id="Gallery">
-        {array.map((item,index) => <Movie key={index}></Movie>)}
+        {array.map((item,index) => <Movie title={index} key={index}></Movie>)}
     </div>
 }
 
--- movie-palace/src/components/Movie.jsx
+++ movie-palace/src/components/Movie.jsx
@@ -1,8 +1,8 @@
-function Movie(){
+function Movie(props){
     const movieObj = {name: "", times:{time1:"12:30",time2:"17:00",time3:"20:00"}}
     return <div id="Movie">
        <img src="" alt="" />
-        Movie Name
+       {props.title}
     </div>
 }
 
