b73f38a9eb1dcbe7092b90e2d832a60c5c1eaeb8
Mac <sophanpa@gmail.com>
2023-07-19 22:04:38 +0200

    Edit

--- movie-palace/src/components/Movie.jsx
+++ movie-palace/src/components/Movie.jsx
@@ -1,11 +1,34 @@
+
+import { useState } from "react";
+
 function Movie(props){
     const movieObj = {name: "", times:{time1:"12:30",time2:"17:00",time3:"20:00"}}
-    return <div id="Movie">
+    const [isActive,setIsActive] = useState(false);
+    function showInfo(){
+            setIsActive(current => !current);
+
+    }
+    let name;
+    let pic;
+    return(
+        <>
+               <div id="Movie" onClick={showInfo} style={{transform: isActive ? "scale(1.2)" : null}}>
        
-        <h1>{props.title}</h1>
-       <img src={props.img} alt="" />
+        <h1>{name = props.title}</h1>
+       <img src={pic = props.img} alt="" />
       
+    </div> 
+
+    <div id="info_background" onClick={showInfo} style={{display: isActive ? "block" : "none"}} ></div>
+    <div id="info_box" style={{display: isActive ? "grid" : "none"}}>
+    <img src={pic} alt="" />
+    <div id="info"><h1>{name}</h1></div>
     </div>
+        </>
+
+    ) 
+
+
 }
 
 export default Movie;
