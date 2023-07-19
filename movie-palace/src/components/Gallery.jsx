718e19237df153e7c014beb664d424ade2bf1f1e
Mac <sophanpa@gmail.com>
2023-07-19 19:40:30 +0200

    Edit

--- movie-palace/src/components/Gallery.jsx
+++ movie-palace/src/components/Gallery.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
 import Movie from "./Movie";
 
 function Gallery(){
-    const array = [{name: "The Flash"},{name: "The Flash"},{name: "The Flash"},{name: "The Flash"},{name: "The Flash"},{name: "The Flash"},{name: "The Flash"},{name: "The Flash"},{name: "The Flash"},{name: "The Flash"}];
+    const [array,setArray] = useState()
+    
+    const url = "https://api.themoviedb.org/3/movie/now_playing?api_key=0d5b49816c94fd70b896c46342342111"
+    useEffect(()=>{
+            async function fetchMovies(){
+    const response = await fetch(url);
+        const movies = await response.json();
+        console.log(movies);
+        setArray(movies.results);
+    }
+    fetchMovies();
+    },[])
+
     return <div id="Gallery">
-        {array.map((item,index) => <Movie title={item.name} key={index}></Movie>)}
+        {array ?  array.map((item,index) => <Movie title={item.title} img={"https://image.tmdb.org/t/p/w500"+item.poster_path} key={index}></Movie>) : null}
+       
     </div>
 }
 
--- movie-palace/src/components/Movie.jsx
+++ movie-palace/src/components/Movie.jsx
@@ -1,8 +1,10 @@
 function Movie(props){
     const movieObj = {name: "", times:{time1:"12:30",time2:"17:00",time3:"20:00"}}
     return <div id="Movie">
-       <img src="" alt="" />
-       {props.title}
+       
+        <h1>{props.title}</h1>
+       <img src={props.img} alt="" />
+      
     </div>
 }
 
